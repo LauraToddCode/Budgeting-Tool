6eb5f873911a171a065ce4208a3a3c5828cc469e
LauraToddCode <laura20todd@gmail.com>
2021-02-07 12:01:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const message = document.getElementById("message")
-const button = document.getElementById("submit")
+const message = document.getElementById("message");
+const button = document.getElementById("submit");
 
 
 
 button.addEventListener("click", function() {
-    let fundAmount = document.getElementById("fundAmount")
-    let item1Amount = document.getElementById("item1Amount")
-    let item2Amount = document.getElementById("item2Amount")
-    let item3Amount = document.getElementById("item3Amount")
+    let fundAmount = document.getElementById("fundAmount");
+    let item1Amount = document.getElementById("item1Amount");
+    let item2Amount = document.getElementById("item2Amount");
+    let item3Amount = document.getElementById("item3Amount");
     
-    let itemsTotal = item1Amount.value + item2Amount.value + item3Amount.value
+    let itemsTotal = item1Amount.value + item2Amount.value + item3Amount.value;
     
     if (fundAmount.value >= itemsTotal) {
-        message.innerHTML = "Go ahead, treat yourself"
+        message.innerHTML = "Go ahead, treat yourself";
     } else {
-        message.innerHTML = "Keep on saving"
+        message.innerHTML = "Keep on saving";
     }
 })
 
-const toggle = document.getElementById("toggle-container")
-const ball = document.getElementById("ball")
-const costs = document.getElementById("costs")
-const budget = document.getElementById("budget")
-const inset = document.getElementById("inset")
-const inputs = document.getElementsByTagName("input")
+const toggle = document.getElementById("toggle-container");
+const ball = document.getElementById("ball");
+const costs = document.getElementById("costs");
+const budget = document.getElementById("budget");
+const inset = document.getElementById("inset");
+const inputs = document.getElementsByTagName("input");
 
 
 toggle.addEventListener("click", function () {
@@ -33,26 +33,26 @@ toggle.addEventListener("click", function () {
     ball.classList.add("moveLeft");
     ball.classList.remove("moveRight");
     setTimeout(() => {
-        document.body.classList.remove("body-darkMode")
+        document.body.classList.remove("body-darkMode");
         costs.classList.remove("boxes-darkMode");
         budget.classList.remove("boxes-darkMode");
         inset.classList.remove("inset-darkMode");
         for (let i = 0; i < inputs.length; i++) {
-            inputs[i].style.backgroundColor = "#FFFFFF"
-            inputs[i].style.color = "#000000"
+            inputs[i].style.backgroundColor = "#FFFFFF";
+            inputs[i].style.color = "#000000";
         }
     }, 350)
   } else {
     ball.classList.add("moveRight");
     ball.classList.remove("moveLeft");
     setTimeout(() => {
-        document.body.classList.add("body-darkMode")
+        document.body.classList.add("body-darkMode");
         costs.classList.add("boxes-darkMode");
         budget.classList.add("boxes-darkMode");
         inset.classList.add("inset-darkMode");
         for (let i = 0; i < inputs.length; i++) {
-            inputs[i].style.backgroundColor = "#2B4257"
-            inputs[i].style.color = "#FFFFFF"
+            inputs[i].style.backgroundColor = "#2B4257";
+            inputs[i].style.color = "#FFFFFF";
         }
     }, 350)     
   }
